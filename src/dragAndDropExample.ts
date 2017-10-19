e9f19b5e148d448d1f77d698190fbfca00a18c56
Yoav Zibin <yzibin@yzibin99.nyc.corp.google.com>
2017-10-19 10:48:41 -0400

    x

---
+++ src/dragAndDropExample.ts
@@ -0,0 +1,80 @@
+module dragAndDrop {
+  function addDragListener(touchElementId: string,
+    handleDragEvent: (type: string, clientX: number, clientY: number, event: TouchEvent | MouseEvent) => void) {
+    if (!touchElementId || !handleDragEvent) {
+      throw new Error("When calling addDragListener(touchElementId, handleDragEvent), you must pass two parameters");
+    }
+
+    let isMouseDown = false;
+
+    function touchHandler(event: TouchEvent) {
+      let touch = event.changedTouches[0];
+      handleEvent(event, event.type, touch.clientX, touch.clientY);
+    }
+
+    function mouseDownHandler(event: MouseEvent) {
+      isMouseDown = true;
+      handleEvent(event, "touchstart", event.clientX, event.clientY);
+    }
+
+    function mouseMoveHandler(event: MouseEvent) {
+      if (isMouseDown) {
+        handleEvent(event, "touchmove", event.clientX, event.clientY);
+      }
+    }
+
+    function mouseUpHandler(event: MouseEvent) {
+      isMouseDown = false;
+      handleEvent(event, "touchend", event.clientX, event.clientY);
+    }
+
+    function handleEvent(event: TouchEvent | MouseEvent, type: string, clientX: number, clientY: number) {
+      // http://stackoverflow.com/questions/3413683/disabling-the-context-menu-on-long-taps-on-android
+      // I also have:  touch-callout:none and user-select:none in main.css
+      if (event.preventDefault) {
+        event.preventDefault(); // Also prevents generating mouse events for touch.
+      }
+      if (event.stopPropagation) {
+        event.stopPropagation();
+      }
+      event.cancelBubble = true;
+      event.returnValue = false;
+      console.log("handleDragEvent:", type, clientX, clientY);
+      handleDragEvent(type, clientX, clientY, event);
+    }
+
+    let gameArea = document.getElementById(touchElementId);
+    if (!gameArea) {
+      throw new Error("You must have <div id='" + touchElementId + "'>...</div>");
+    }
+    gameArea.addEventListener("touchstart", touchHandler, true);
+    gameArea.addEventListener("touchmove", touchHandler, true);
+    gameArea.addEventListener("touchend", touchHandler, true);
+    gameArea.addEventListener("touchcancel", touchHandler, true);
+    gameArea.addEventListener("touchleave", touchHandler, true);
+    gameArea.addEventListener("mousedown", mouseDownHandler, true);
+    gameArea.addEventListener("mousemove", mouseMoveHandler, true);
+    gameArea.addEventListener("mouseup", mouseUpHandler, true);
+  }
+
+  function init() {
+    let currentElement: HTMLElement = null;
+    let currentZIndex = 100;
+    addDragListener('draggableArea', (type: string, clientX: number, clientY: number, event: TouchEvent | MouseEvent) => {
+      if (type == "touchstart") {
+        currentElement = <HTMLElement> document.elementFromPoint(clientX, clientY);
+        if (!currentElement.classList.contains('draggable')) currentElement = null;
+      }
+      if (currentElement != null) {
+        currentElement.style.left = clientX + 'px';
+        currentElement.style.top = clientY + 'px';
+        currentElement.style.zIndex = '' + (++currentZIndex);
+      }
+      if (type == "touchend") {
+        currentElement = null;
+      }
+    });
+  }
+
+  init();
+}

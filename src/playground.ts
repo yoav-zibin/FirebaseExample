2cbbc905b731903d07129045338497fcdef4e39f
yoav_zibin <yoav.zibin@gmail.com>
2018-03-02 15:38:36 -0500

    x

---
+++ src/playground.ts
@@ -0,0 +1,14 @@
+
+  let config = {
+    apiKey: `AIzaSyA_UNWBNj7zXrrwMYq49aUaSQqygDg66SI`,
+    authDomain: `testproject-a6dce.firebaseapp.com`,
+    databaseURL: `https://testproject-a6dce.firebaseio.com`,
+    projectId: `testproject-a6dce`,
+    storageBucket: ``,
+    messagingSenderId: `957323548528`
+  };
+  firebase.initializeApp(config);
+  firebase.auth().signInAnonymously()
+  .then(function(result) {
+    console.info("signInAnonymously succeded! My user id is: ", result.uid);
+  })

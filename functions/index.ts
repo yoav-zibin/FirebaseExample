ec3a5d08cbc6f78a306c61ac55c40758f10334ce
yoav_zibin <yoav.zibin@gmail.com>
2018-04-25 10:19:47 -0400

    YZ:

--- functions/index.ts
+++ functions/index.ts
@@ -25,8 +25,26 @@ admin.initializeApp(functions.config().firebase);
 //  response.send("Hello from Firebase!");
 // });
 
-// TODO: implement push notification when someone writes to
+// TODO: implement push notification.
+// We will have 2 types of notifications:
+//
+// 1) When someone added you as a participant, i.e., when someone writes to:
+// /gamePortal/gamePortalUsers/${userId}/privateButAddable/matchMemberships/${matchId}/addedByUid
+// then it will send a push notification to ${userId} with the text:
+// {title: "<user-name> added you to a game of <Monopoly>", body: "Open Zibiga now to join the fun!"}
+// <user-name> will be either the contact name or displayName of addedByUid, with preference to contact name.
+// Recall that contact names are stored in /privateFields/contacts/$contactPhoneNumber/contactName
+// Recall that displayName are stored in /publicFields/displayName
+//
+// 2) When someone writes to
 // /gamePortal/matches/$matchId/participants/$participantUserId/pingOpponents
+// Suppose we have a match between users A, B, C.
+// When user A writes to:
+// /gamePortal/matches/$matchId/participants/<userId-of-A>/pingOpponents
+// Then it will send a notifications to users B & C, with the text:
+// {title: "<user-name> resumes the game of <Monopoly>", body: "Open Zibiga now to join the fun!"}
+// As before, <user-name> is either the contact name or displayName.
+
 
 
 exports.testPushNotification =
@@ -40,12 +58,11 @@ functions.database.ref('testPushNotification').onWrite((event: any) => {
       {
         notification: {
           title: "Monopoly game is starting",
-          body: "Open GamePortal and join the fun!",
+          body: "Open Zibiga and join the fun!",
         },
         data: {
           // Must be only strings in these key-value pairs
           examplePayload: "Some arbitrary payload string",
-          fcmToken: fcmToken,
         }
       };
   return admin.messaging().sendToDevice([fcmToken], payload)
